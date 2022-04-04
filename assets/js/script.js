422436f7b8d77d96be490010fe55e3470c5fb5e4
adrianapvent <adriana.e.pena@gmail.com>
2022-04-04 09:48:21 -0500

    fixed bug errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,68 @@
 //WHEN I open the planner THEN the current day is displayed at the top of the calendar
 // reference #currentDay, add moment().format('MMMM Do YYYY, h:mm:ss a');, display it
-const timeStamp = function () {
-    let date = moment().format('MMMM Do YYYY');
-    currentDayEl.textContent = date;
-};
-// reference .container, create list, apply 9am to 5pm
-/* const displayTimeBlocks = function (routine, description) { // create time?
-    var createRow = $("<div>") // parent
-        .addClass("row");
-    var timeBlock = $("<p>") // child 1 // create timeblock
-        .addClass("time-block")
-        .text(routine);
-    var eventDescription = $("<p>") // child 2 // create description next to timeblock
-        .addClass("description")
-        .text(description);
-    // append two children to parent
-    createRow.append(timeBlock, eventDescription);
-    console.log(createRow);
-    // append parent to HTML reference
-    createRow.append(timeBlocksEl);
-}; */
-//references
-const currentDayEl = document.querySelector("#currentDay");
-const timeBlocksEl = document.querySelector(".container");
-timeStamp();
 
+var date = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(date);
+
+$(document).ready(function () {
+    // saveBtn click listener
+    $(".saveBtn").on("click", function () {
+        // get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+// })
+
+    function timeTracker() {
+        //get current number of hours
+        var timeNow = moment().hour();
+
+        //loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To chck the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+// Get item from local storage if any
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+// //references
+// const currentDayEl = document.querySelector("#currentDay");
+// const timeBlocksEl = document.querySelector(".container");
+// timeStamp();
+
+timeTracker();
+
+})
 
 
 
